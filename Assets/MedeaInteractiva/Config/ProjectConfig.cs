2491e8eedbd65d6c220f37c8440ea81bcc0326b0
freds <jfr3dy90@gmail.com>
2024-02-27 21:32:17 -0500

    platform config

---
+++ Assets/MedeaInteractiva/Config/ProjectConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public  class ProjectConfig : MonoBehaviour
+{
+    public static ProjectConfig Instance;
+    public Platform actualPlatform;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
+
+public enum Platform
+{
+    WebGl,
+    Android,
+    Oculus
+}
